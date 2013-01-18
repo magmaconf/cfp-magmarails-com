c4a806ff3ce0d5c61b15b965405aca0604786f07
David Padilla <david@crowdint.com>
2013-01-18 12:29:15 -0600

    Show the profile email on the proposal detail page

--- config/initializers/injections.rb
+++ config/initializers/injections.rb
@@ -4,3 +4,9 @@
 
 Cfp::Profile.send :attr_accessible, :email
 Cfp::Profile.send :validates, :email, :presence => true
+
+Cfp::Proposal.class_eval do
+  def user_email
+    user.profile.email
+  end
+end
