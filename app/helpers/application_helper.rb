33abe9fd52180ccf0054141171ccc40333b50b93
Hector Bustillos <hector.bustillos@crowdint.com>
2013-02-06 11:56:22 -0600

    Add prev and next links to discuss view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,14 @@ module ApplicationHelper
     end
   end
 
+  def prev_help(id)
+    prev_proposal = Cfp::Proposal.where(id: id - 1)
+    link_to(raw("<i class='icon-arrow-left'></i> Prev"), proposal_comments_path(prev_proposal.first.id), class: 'pull-left' )  if prev_proposal.first
+  end
+
+  def next_help(id)
+    next_proposal = Cfp::Proposal.where(id: id + 1)
+    link_to(raw("Next <i class='icon-arrow-right'></i>"), proposal_comments_path(next_proposal.first.id), class: 'pull-right' )  if next_proposal.first
+  end
+
 end
