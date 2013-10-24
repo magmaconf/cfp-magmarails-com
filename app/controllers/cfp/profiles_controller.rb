7ce0c474be8ccd308733f52df6b228bcb85fa897
Giovanni Mendoza <mendozagioo@gmail.com>
2013-10-24 14:25:04 -0500

    [##1159471] CFP - Remove Profile Show
    * Override profile controller create/update to redirect to root
    * Remove show view

---
+++ app/controllers/cfp/profiles_controller.rb
@@ -0,0 +1,23 @@
+require_dependency Cfp::Engine.root.join('app', 'controllers', 'cfp', 'profiles_controller').to_s
+
+Cfp::ProfilesController.class_eval do
+  def create
+    @profile = Cfp::Profile.new profile_params
+    if @profile.save
+      current_user.profile = @profile
+      current_user.save
+      redirect_to '/'
+    else
+      render :action => :new
+    end
+  end
+
+  def update
+    if @profile.update_attributes profile_params
+      redirect_to '/', :notice => t('profile.edited.success')
+    else
+      render :action => :edit
+    end
+  end
+
+end
