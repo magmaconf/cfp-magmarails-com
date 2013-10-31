7bbf9538e23753045306f06b40e4f27bde27de8c
David Padilla <david@crowdint.com>
2013-10-31 16:49:36 -0600

    Set the correct CFP end date

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,7 +17,7 @@ module ApplicationHelper
   end
 
   def remaining_days
-    date_to_go = (Date.new(2013, 11, 5) - Date.today).to_i
+    date_to_go = (Date.new(2013, 12, 31) - Date.today).to_i
     date_to_go.to_s.rjust(2, '0')
   end
 
