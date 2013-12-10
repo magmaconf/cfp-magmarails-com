fe46221f56840ac17bdde25ca724d933be8012a3
David Padilla <david@crowdint.com>
2013-12-10 10:08:49 -0600

    New decorators and methods to see pending talks

--- app/decorators/models/cfp/proposal_decorator.rb
+++ app/decorators/models/cfp/proposal_decorator.rb
@@ -1,7 +1,9 @@
-Cfp::Proposal
-
 class Cfp::Proposal < ActiveRecord::Base
   after_create do
     SubmissionMailer.submitted(self).deliver
+  end
+
+  def self.pending_titles
+    where(approved: false).collect { |p| [p.id, p.title] }
   end
 end
