35dd7a916d4c6f5ed1a6193c6c97397e9cb0264a
David Padilla <david@crowdint.com>
2014-01-03 21:42:21 -0600

    Methods to rank the talks

--- app/decorators/models/cfp/proposal_decorator.rb
+++ app/decorators/models/cfp/proposal_decorator.rb
@@ -6,4 +6,21 @@ class Cfp::Proposal < ActiveRecord::Base
   def self.pending_titles
     where(approved: false).collect { |p| [p.id, p.title] }
   end
+
+  # TODO: Code Below is horrible and should probably be handled
+  # with counter caches directly on the cfp gem.
+  #
+  # Still, serves its purpose.
+  #
+  def self.ranked_and_sorted
+    all.to_a.inject([]) {
+      |m,v| m << {talk: v, rank: v.average_ranking}; m
+    }.sort { |a,b| b[:rank] <=> a[:rank] }
+  end
+
+  def self.ranked_and_sorted_titles
+    all.to_a.inject([]) {
+      |m,v| m << {talk: v.title, rank: v.average_ranking}; m
+    }.sort { |a,b| b[:rank] <=> a[:rank] }
+  end
 end
