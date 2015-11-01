d0aa5df099023922404a038a988b39d56ca12854
Ignacio Galindo <joiggama@gmail.com>
2015-11-01 12:32:54 -0600

    Add configurable contact email
    - Add no-reply@magmaconf.com as default mail sender (from: don't send emails on behalf of others).
    - Format mail view
    - Use environment variable CFP_CONTACT_EMAIL mail recipient (to:)

--- app/mailers/contact.rb
+++ app/mailers/contact.rb
@@ -1,12 +1,13 @@
 class Contact < ActionMailer::Base
-  default from: "info@magmaconf.com"
+  default from: "no-reply@magmaconf.com"
+  default to: ENV["CFP_CONTACT_EMAIL"]
 
   def send_mail name, company, email
     @name = name
     @email = email
     @company = company
 
-    mail(from: email, subject: 'Sponsor inquiry')
+    mail(subject: 'Sponsor inquiry')
   end
 
 
