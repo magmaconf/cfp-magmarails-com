2de494dde29bcc3858681add433df5b53c219ebe
Ignacio Galindo <joiggama@gmail.com>
2015-11-09 20:00:39 -0600

    Upgrade sivem.js (unminified)

--- vendor/assets/javascripts/civem.js
+++ vendor/assets/javascripts/civem.js
@@ -1,11 +1,82 @@
 /**
  * @preserve
  * 
- * CIVEM v0.0.7
+ * {{= it.title}} v{{= it.version}}
  *
- * Copyright 2012, Hannu Leinonen <hleinone@gmail.com>
+ * Copyright 2012, {{= it.author.name}} <{{= it.author.email}}>
  * Dual licensed under the MIT and GPL licenses:
  *   http://www.opensource.org/licenses/mit-license.php
  *   http://www.gnu.org/licenses/gpl.html
  */
-(function(){function t(t){return function(e){if("RADIO"===e.target.type.toUpperCase()){var a=document.getElementsByName(e.target.name);for(i=0;a.length>i;i++)a[i].setCustomValidity("")}else e.target.setCustomValidity("");t&&t(e),e.target.checkValidity()}}function e(t){return function(e){var a,n,o=e.target,i=o.validity,r=i.valueMissing?"value-missing":i.typeMismatch?"type-mismatch":i.patternMismatch?"pattern-mismatch":i.tooLong?"too-long":i.rangeUnderflow?"range-underflow":i.rangeOverflow?"range-overflow":i.stepMismatch?"step-mismatch":i.customError?"custom-error":"";r&&(a=o.getAttribute("data-errormessage-"+r))?o.setCustomValidity(a):(n=o.getAttribute("data-errormessage"))?o.setCustomValidity(n):o.setCustomValidity(o.validationMessage),t&&t(e)}}var a=function(){var a,n=[],o=document.getElementsByTagName("input");for(a=0;o.length>a;a++)n.push(o[a]);var i=document.getElementsByTagName("textarea");for(a=0;i.length>a;a++)n.push(i[a]);var r=document.getElementsByTagName("select");for(a=0;r.length>a;a++)n.push(r[a]);for(a=0;n.length>a;a++){var s=n[a];s.willValidate&&(console.log(s.tagName),"SELECT"===s.tagName.toUpperCase()||"RADIO"===s.type.toUpperCase()||"CHECKBOX"===s.type.toUpperCase()?s.onchange=t(s.onchange):s.oninput=t(s.oninput),s.oninvalid=e(s.oninvalid))}};window.addEventListener?window.addEventListener("load",a,!1):window.attachEvent&&window.attachEvent("onload",a)})();
+(function() {
+	function getInputHandler(inputCallback) {
+		return function(event) {
+			if (event.target.type.toUpperCase() === "RADIO") {
+				var radioGroup = document.getElementsByName(event.target.name)
+				for (i = 0; i < radioGroup.length; i++) {
+					radioGroup[i].setCustomValidity("");
+				}
+			} else {
+				event.target.setCustomValidity("");
+			}
+			if (inputCallback) {
+				inputCallback(event);
+			}
+			event.target.checkValidity();
+		};
+	}
+
+	function getInvalidHandler(invalidCallback) {
+		return function(event) {
+			var element = event.target;
+			var validity = element.validity;
+			var suffix = validity.valueMissing ? "value-missing" : validity.typeMismatch ? "type-mismatch" : validity.patternMismatch ? "pattern-mismatch" : validity.tooLong ? "too-long" : validity.rangeUnderflow ? "range-underflow" : validity.rangeOverflow ? "range-overflow" : validity.stepMismatch ? "step-mismatch" : validity.customError ? "custom-error" : "";
+			var specificErrormessage, genericErrormessage;
+			if (suffix && (specificErrormessage = element.getAttribute("data-errormessage-" + suffix))) {
+				element.setCustomValidity(specificErrormessage);
+			} else if (genericErrormessage = element.getAttribute("data-errormessage")) {
+				element.setCustomValidity(genericErrormessage);
+			} else {
+				element.setCustomValidity(element.validationMessage);
+			}
+			if (invalidCallback) {
+				invalidCallback(event);
+			}
+		};
+	}
+
+	var civem = function() {
+		var formElements = [];
+		var inputElements = document.getElementsByTagName("input");
+		var i;
+		for (i = 0; i < inputElements.length; i++) {
+			formElements.push(inputElements[i]);
+		}
+		var textareaElements = document.getElementsByTagName("textarea");
+		for (i = 0; i < textareaElements.length; i++) {
+			formElements.push(textareaElements[i]);
+		}
+		var selectElements = document.getElementsByTagName("select");
+		for (i = 0; i < selectElements.length; i++) {
+			formElements.push(selectElements[i]);
+		}
+		for (i = 0; i < formElements.length; i++) {
+			var formElement = formElements[i];
+			if (!formElement.willValidate) {
+				continue;
+			}
+			if (formElement.tagName.toUpperCase() === "SELECT" || formElement.type.toUpperCase() === "RADIO" || formElement.type.toUpperCase() === "CHECKBOX") {
+				formElement.onchange = getInputHandler(formElement.onchange);
+			} else {
+				formElement.oninput = getInputHandler(formElement.oninput);
+			}
+			formElement.oninvalid = getInvalidHandler(formElement.oninvalid);
+		}
+	};
+
+	if (window.addEventListener) {
+		window.addEventListener("load", civem, false);
+	} else if (window.attachEvent) {
+		window.attachEvent("onload", civem);
+	}
+}());
