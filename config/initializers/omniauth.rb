6746c0bb11fea36861978be8f32287036bd5b4ea
Ignacio Galindo <joiggama@gmail.com>
2015-11-18 11:34:19 -0600

    Let's use SSL on development

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, ENV['CFP_APP_ID'], ENV['CFP_APP_SECRET']
 end
+
+if Rails.env.development?
+  OmniAuth.config.full_host = "https://cfp.magmaconf.dev"
+end
