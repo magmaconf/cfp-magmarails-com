e0caca687f9243a7f6e4f7f152ebe6d6f09fe9d9
Ignacio Galindo <joiggama@gmail.com>
2015-11-20 02:34:36 -0600

    WIP: Rate proposals design update. Proposal details.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def already_reviewed(proposal)
-    content_tag 'i','', class: 'fa fa-check raked' if Cfp::Rank.for_proposal_user(proposal, current_user) && current_user.can_review?
+    image_tag 'read_icon.png' if Cfp::Rank.for_proposal_user(proposal, current_user) && current_user.can_review?
   end
 
   def reviewed(proposal)
