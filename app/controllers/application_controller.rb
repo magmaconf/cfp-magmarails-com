35f1305b3838c890e73d32bbfe44f120d75b4b6c
Ignacio Galindo <joiggama@gmail.com>
2016-01-12 23:43:42 +0100

    Remove activerecord-deprecated_finders dependency

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+
   protect_from_forgery
 
   before_filter :authenticate_user!
@@ -7,13 +8,12 @@ class ApplicationController < ActionController::Base
 
   force_ssl
 
-
   def load_settings
     Cfp::Config.load_from_persistance if Rails.env.development?
   end
 
   def load_talks
-    @propos = Cfp::Proposal.approved.paginate(page: params[:page], per_page: 10, :order => "created_at DESC")
+    @propos = Cfp::Proposal.approved.order("created_at desc").limit(10).page(params[:page])
     @propos_phone = Cfp::Proposal.approved.reverse
   end
 
